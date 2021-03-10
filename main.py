0711aa797dc2fe40e52d32215b16db466abec1fe
Taylor Cooke <taylorcooke@gmail.com>
2021-03-10 13:33:14 -0500

    plotting data

--- main.py
+++ main.py
@@ -13,5 +13,9 @@ worksheets = wb.sheet_names
 
 feb = worksheets[4]
 feb_frame = pd.read_excel(wb, feb)
+feb_frame.rename(columns={datetime(2021, 2, 1, 0, 0): 'F-21'}, inplace = True)
 
-print(feb_frame.head())
+# print(feb_frame.columns)
+
+fig = px.line(feb_frame, x='Day', y='F-21', title='over time')
+fig.show()
